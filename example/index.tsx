7030e8aaf07fa969f9411cca78fc0a8fea32ea93
Lukas Boehler <lukas@boehlerbrothers.com>
2022-01-31 19:40:33 +0100

    v6.3.5

--- example/index.tsx
+++ example/index.tsx
@@ -4,6 +4,6 @@ import Gleap from 'react-native-gleapsdk';
 import { name as appName } from './app.json';
 
 Gleap.enableDebugConsoleLog();
-Gleap.initialize('wytzEhhSa1EFfTEqK3HXBWuGRt2PREAE');
+Gleap.initialize('Y0ASDsS3Se1PJG1aYNIblrFMMX4zGgig');
 
 AppRegistry.registerComponent(appName, () => App);
