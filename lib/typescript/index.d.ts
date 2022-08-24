2798ee6ee845c297dc63137d226e8af83de9f896
Josh Norman <xnor@deako.com>
2022-08-24 12:25:35 -0700

    Adding updated library for send crash report name change

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,34 +1,54 @@
 export declare type GleapUserProperty = {
     email?: string;
     name?: string;
+    phone?: string;
+    value?: number;
 };
 declare type GleapActivationMethod = 'SHAKE' | 'SCREENSHOT';
 declare type GleapSdkType = {
     initialize(token: string): void;
+    startFeedbackFlow(feedbackFlow: string, showBackButton: boolean): void;
+    sendSilentCrashReport(description: string, severity: 'LOW' | 'MEDIUM' | 'HIGH'): void;
+    sendSilentCrashReportWithExcludeData(description: string, severity: 'LOW' | 'MEDIUM' | 'HIGH', excludeData: {
+        customData?: Boolean;
+        metaData?: Boolean;
+        attachments?: Boolean;
+        consoleLog?: Boolean;
+        networkLogs?: Boolean;
+        customEventLog?: Boolean;
+        screenshot?: Boolean;
+        replays?: Boolean;
+    }): void;
     open(): void;
-    startFeedbackFlow(feedbackFlow: string): void;
-    sendSilentBugReport(description: string, severity: 'LOW' | 'MEDIUM' | 'HIGH'): void;
-    sendSilentBugReportWithType(description: string, severity: 'LOW' | 'MEDIUM' | 'HIGH', type: string): void;
+    close(): void;
+    isOpened(): boolean;
     identify(userId: string, userProperties: GleapUserProperty): void;
+    identifyWithUserHash(userId: string, userProperties: GleapUserProperty, userHash: string): void;
     clearIdentity(): void;
+    preFillForm(formData: {
+        [key: string]: string;
+    }): void;
     setApiUrl(apiUrl: string): void;
-    setWidgetUrl(widgetUrl: string): void;
+    setFrameUrl(frameUrl: string): void;
     attachCustomData(customData: any): void;
     setCustomData(key: string, value: string): void;
     removeCustomDataForKey(key: string): void;
     clearCustomData(): void;
-    registerCustomAction(customActionCallback: (data: {
-        name: string;
-    }) => void): void;
     registerListener(eventType: string, callback: (data?: any) => void): void;
     setLanguage(language: string): void;
+    enableDebugConsoleLog(): void;
+    disableConsoleLog(): void;
+    log(message: string): void;
+    logWithLogLevel(message: string, logLevel: 'INFO' | 'WARNING' | 'ERROR'): void;
     logEvent(name: string, data: any): void;
     addAttachment(base64file: string, fileName: string): void;
     removeAllAttachments(): void;
     startNetworkLogging(): void;
     stopNetworkLogging(): void;
-    enableDebugConsoleLog(): void;
     setActivationMethods(activationMethods: GleapActivationMethod[]): void;
+    registerCustomAction(customActionCallback: (data: {
+        name: string;
+    }) => void): void;
 };
 declare const _default: GleapSdkType;
 export default _default;
