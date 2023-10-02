d06ae1f13db804b92d41e56af0b81a1f26b4392a
Lukas Böhler <lukas@boehlerbrothers.com>
2023-10-02 10:51:46 +0200

    v12.0.2

--- example/index.js
+++ example/index.js
@@ -2,11 +2,20 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import Gleap from 'react-native-gleapsdk';
 
 Gleap.initialize('X5C0grjFCjUMbZKi131MjZLaGRwg2iKH');
+
+Gleap.registerListener('customActionTriggered', (data) => {
+    console.log("data", data);
+    if (data.name === 'CUSTOM_HELP') {
+        setTimeout(() => {
+            Gleap?.openHelpCenterCollection("1", true);
+        }, 1500);
+    }
+});
 
 AppRegistry.registerComponent(appName, () => App);
